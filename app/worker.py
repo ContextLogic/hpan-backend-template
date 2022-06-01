470391c9a81da92b3217ea1651aae26be176b024
hpan-wish <hpan@wish.com>
2022-05-31 18:12:46 -0700

    Initialize fix_readme

---
+++ app/worker.py
@@ -0,0 +1,19 @@
+"""
+main script to create a worker, configure the worker and start it.
+"""
+
+from app.config.config import Config, WorkerConfig
+from app.config.load import load_options_from_cli
+from base_be_worker.worker import Worker
+
+from app.tasks import register_tasks
+
+if __name__ == "__main__":
+    load_options_from_cli()
+    worker = Worker(
+        "all",
+        celery_worker_config=WorkerConfig,
+        task_register_func=register_tasks,
+        **vars(Config)
+    )
+    worker.start()
