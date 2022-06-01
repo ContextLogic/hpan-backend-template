3662c67364f260fb402318a4cc3695a86df5f08c
hpan-wish <hpan@wish.com>
2022-05-31 18:12:48 -0700

    Initialize rgao-dlq

---
+++ app/tasks/__init__.py
@@ -0,0 +1,12 @@
+"""
+tasks module provides a function to register all tasks to celery app.
+"""
+
+
+def register_tasks() -> None:
+    """
+    register tasks to the current celery app
+    """
+    # pylint: disable=import-outside-toplevel,unused-import
+    from .function_based_tasks import retry_demo, err_retry_demo, add
+    from .polymorphism import polymorphism_task
